a4560d3866749f5152e1cf7eb286f5feb4030311
ViolinistaEstudante <132099734+ViolinistaEstudante@users.noreply.github.com>
2024-08-26 21:42:01 -0300

    Site concluído!!

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    window.open('https://www.roblox.com/games/17333170701/Simulador-de-nibus-rodovi-rios-BETAO', '_blank');
+});
